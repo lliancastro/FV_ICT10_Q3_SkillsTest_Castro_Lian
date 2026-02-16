0441ca21efcf1f66968333eb7052fdd60a42a4d2
lliancastro <lianc4793@gmail.com>
2026-02-16 12:28:02 +0800

    Add files via upload

---
+++ ii.py
@@ -0,0 +1,7 @@
+from pyscript import display
+
+def account_creation(e):
+    document.getElementById('output').innerHTML= ""
+
+    username = document.getElementById('Username')
+    password = document.getElementById('Password')
